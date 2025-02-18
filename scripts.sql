911c879f325c1849a142d06be7c9d3c323dfb0be
jesusrevilla <jesus_revilla@yahoo.com>
2025-02-17 21:54:00 -0600

    Add schema

--- scripts.sql
+++ scripts.sql
@@ -1,22 +1,30 @@
 -- create table clientes (id, email)
-
-
-
-
+CREATE TABLE clientes (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(30) NOT NULL UNIQUE
+)
 
 
 -- create table facturas (id, client_id)
-
-
-
+CREATE TABLE facturas(
+    id SERIAL PRIMARY KEY,
+    client_id INTEGER NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES clientes (id)
+)
 
 
 
 -- create table productos (id, nombre, precio)
-
-
-
-
+CREATE TABLE productos(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(40) NOT NULL,
+    precio NUMERIC CHECK(precio > 10)
+)
 
 
 -- create table envios (id, fecha, estado)
+CREATE TABLE envios (
+    id SERIAL PRIMARY KEY,
+    fecha DATE,
+    estado VARCHAR CHECK(estado IN ('preparando', 'en tránsito', 'entregado'))
+)
