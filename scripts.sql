c2a2171a996828c3edec3a9a7428d9c28f489bc8
170973 <170973@upslp.edu.mx>
2025-02-19 11:16:35 -0600

    Update scripts.sql 170973

--- scripts.sql
+++ scripts.sql
@@ -1,1 +1,34 @@
+CREATE TABLE empleados (
+  Id SERIAL PRIMARY KEY,
+  nombre  VARCHAR(100) NOT NULL,
+  salario NUMERIC
+);
+
+--INSERTAR LIBROS
+INSERT INTO empleados (nombre, salario) VALUES
+('Juan Perez', 50000),
+('Ana Gómez', 60000),
+('Luis Martinez', 55000),
+('Adriana Martinez', 65000),
+('Susana Montoya', 55000);
+
+--mostrar tabla de empleados original 
+SELECT * FROM empleados;
+
+--modificacion 1 de tabla 
+UPDATE empleados SET salario = 55000 
+WHERE nombre = 'Ana Gómez';
+SELECT * FROM empleados;
+
+-- eliminacion de registro 
+DELETE FROM empleados
+WHERE nombre = 'Luis Martinez';
+SELECT * FROM empleados;
+
+--mostrar empleados con sueldo mayor a los 50 000
+SELECT * FROM empleados WHERE salario  > 50000;
+
+--tabla  con solo nombres
+SELECT nombre FROM empleados;
+
 
