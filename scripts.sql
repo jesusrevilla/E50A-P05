c441c2d282ac845b7eb4353c3f09a4d9e015f957
ale <88249659+sopadepollo@users.noreply.github.com>
2025-02-18 18:47:11 -0600

    Update scripts.sql 177890

--- scripts.sql
+++ scripts.sql
@@ -1,1 +1,23 @@
 
+-- create
+CREATE TABLE empleados (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL,
+  salario NUMERIC NOT NULL
+);
+
+-- insert
+INSERT INTO empleados (nombre, salario) VALUES ('Juan Pérez', 50000);
+INSERT INTO empleados (nombre, salario) VALUES ('Ana Gómez', 60000);
+INSERT INTO empleados (nombre, salario) VALUES ('Luis Martínez', 55000);
+INSERT INTO empleados (nombre, salario) VALUES ('Adriana Martínez', 65000);
+INSERT INTO empleados (nombre, salario) VALUES ('Susana Montoya', 55000);
+
+UPDATE empleados SET salario = 65000 WHERE nombre='Ana Gómez';
+DELETE FROM empleados WHERE nombre = 'Luis Martínez';
+
+-- fetch 
+SELECT * FROM empleados;
+SELECT * FROM empleados WHERE salario > 50000;
+SELECT nombre FROM empleados;
+
