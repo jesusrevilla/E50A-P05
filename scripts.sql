304ac28821d7e351960a9a8844c1dc9b69dff120
vulletz <122696042+vulletz@users.noreply.github.com>
2025-02-18 18:26:13 -0600

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,1 +1,27 @@
+\echo 'Sebastian Heredia Pardo - 175680'
 
+-- Creacion de tabla empleados
+CREATE TABLE empleados (
+ id SERIAL PRIMARY KEY,
+ nombre VARCHAR(100),
+ salario NUMERIC
+);
+
+INSERT INTO empleados (nombre, salario) VALUES 
+('Juan Pérez', 50000),
+('Ana Gómez', 60000),
+('Luis Martínez', 55000),
+('Adriana Martínez', 65000),
+('Susana Montoya', 55000);
+
+--SELECT * FROM empleados;
+
+UPDATE empleados SET salario = 65000 WHERE nombre = 'Ana Gómez';
+
+--SELECT * FROM empleados;
+
+DELETE FROM empleados WHERE nombre = 'Luis Martínez';
+
+SELECT * FROM empleados;
+SELECT * FROM empleados WHERE salario > 50000;
+SELECT nombre FROM empleados;
