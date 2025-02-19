8bf9001e5ff2c92a0b1ae5fb5d9f1b56bae480df
AntonioMoralesQuiroz <176412@upslp.edu.mx>
2025-02-18 18:48:03 -0600

    176412.sql

--- scripts.sql
+++ scripts.sql
@@ -1,1 +1,29 @@
 
+-- create
+CREATE TABLE empleados (
+ id SERIAL PRIMARY KEY,
+ nombre VARCHAR(100),
+ salario NUMERIC
+);
+
+
+INSERT INTO empleados (nombre, salario) VALUES ('Juan Pérez', 50000);
+INSERT INTO empleados (nombre, salario) VALUES ('Ana Gómez', 60000);
+INSERT INTO empleados (nombre, salario) VALUES ('Luis Martínez', 55000);
+INSERT INTO empleados (nombre, salario) VALUES ('Adriana Martínez', 65000);
+INSERT INTO empleados (nombre, salario) VALUES ('Susana Montoya', 55000);
+
+
+
+SELECT * FROM empleados;
+
+
+
+UPDATE empleados SET salario = 65000 WHERE  nombre = 'Ana Gómez';
+
+
+
+DELETE FROM empleados WHERE nombre = 'Luis Martínez';
+
+SELECT * FROM empleados;
+
