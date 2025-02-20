cb288f885abf697051229371b0f55935b07bc8ef
Alfredo De Alba Sánchez <dealbaa347@gmail.com>
2025-02-19 18:10:57 -0600

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,1 +1,16 @@
 
+CREATE TABLE empleados (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100),
+  salario NUMERIC
+);
+
+INSERT INTO empleados(nombre,salario) VALUES
+ ('Juan Pérez', 50000),
+ ('Ana Gómez', 60000),
+ ('Luis Martínez', 55000),
+ ('Adriana Martínez', 65000),
+ ('Susana Montoya', 55000);
+
+UPDATE empleados SET salario=65000 WHERE nombre='Ana Gómez';
+DELETE FROM empleados WHERE nombre='Luis Martínez';
